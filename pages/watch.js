87779fb8d64d49d1e390e90ad6c4efb0dcaef364
Feross Aboukhadijeh <feross@feross.org>
2020-09-04 15:43:21 -0700

    standard

--- pages/watch.js
+++ pages/watch.js
@@ -4,30 +4,17 @@ import { useEffect } from 'react'
 import loadScript from 'load-script2'
 
 import {
-  Avatar,
   Box,
-  Container,
-  Heading,
-  Stack,
-  Text,
-  Wrap,
-  useColorModeValue
+  Container
 } from '@chakra-ui/core'
 
-import { ButtonLink } from '../components/ButtonLink'
-import { ColorModeButton } from '../components/ColorModeButton'
-import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
-import { Link } from '../components/Link'
-
-import { colorScheme } from '../theme'
-import events from '../events'
 
 const WatchPage = ({ events }) => {
   useEffect(() => {
     ; (async () => {
       await loadScript('https://embed.twitch.tv/embed/v1.js')
-      const twitch = new Twitch.Embed('twitch-embed', {
+      window.twitch = new Twitch.Embed('twitch-embed', {
         width: 1024,
         height: 600,
         channel: 'speakeasyjs'
