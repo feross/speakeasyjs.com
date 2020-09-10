7f4279623db96ecc9979075c7e1856e2ccb18da4
Feross Aboukhadijeh <feross@feross.org>
2020-09-09 17:59:44 -0700

    typo

--- events.js
+++ events.js
@@ -32,7 +32,7 @@ module.exports = [
       },
       {
         time: '4:20',
-        title: 'Indexing P2P Sites with Hyperbee"',
+        title: 'Indexing P2P Sites with Hyperbee',
         name: 'Andrew Osheroff',
         twitter: 'andrewosh'
       },
