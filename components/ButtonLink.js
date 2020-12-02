9d00f889246574b48c6a4732e167d777fbba0c03
Feross Aboukhadijeh <feross@feross.org>
2020-12-01 16:04:26 -0800

    remove nextRoute

--- components/ButtonLink.js
+++ components/ButtonLink.js
@@ -2,18 +2,26 @@ import { Button } from '@chakra-ui/react'
 
 import { Link } from './Link'
 
-export const ButtonLink = ({ href, nextRoute, ...rest }) => (
-  <Link
-    href={href}
-    nextRoute={nextRoute}
-    showExternalIcon={false}
-    textDecoration='none'
-    _hover={{
-      textDecoration: 'none'
-    }}
-  >
-    <Button {...rest} />
-  </Link>
-)
+export const ButtonLink = ({
+  as = Button,
+  href,
+  prefetch,
+  ...rest
+}) => {
+  const ButtonComponent = as
+  return (
+    <Link
+      href={href}
+      prefetch={prefetch}
+      showExternalIcon={false}
+      textDecoration='none'
+      _hover={{
+        textDecoration: 'none'
+      }}
+    >
+      <ButtonComponent {...rest} />
+    </Link>
+  )
+}
 
 export default ButtonLink
--- components/Link.js
+++ components/Link.js
@@ -6,8 +6,7 @@ import { ExternalLinkIcon } from './icons'
 const EXAMPLE_ORIGIN = 'http://example.com:9999'
 
 export const Link = ({
-  as = null,
-  nextRoute = null,
+  as = ChakraLink,
   href,
   showExternalIcon,
   children,
@@ -17,13 +16,13 @@ export const Link = ({
   const url = new URL(href, EXAMPLE_ORIGIN)
   const isExternal = url.origin !== EXAMPLE_ORIGIN
 
-  const LinkComponent = as || ChakraLink
-
   if (showExternalIcon == null) showExternalIcon = isExternal
 
   const externalIcon = showExternalIcon
     ? <ExternalLinkIcon ml={2} />
     : null
+
+  const LinkComponent = as
 
   if (isExternal) {
     return (
@@ -38,8 +37,7 @@ export const Link = ({
     if (prefetch === false) nextLinkProps.prefetch = false
     return (
       <NextLink
-        href={nextRoute ?? href}
-        as={nextRoute ? href : null}
+        href={href}
         passHref
         {...nextLinkProps}
       >
